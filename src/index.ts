97b3e9b70c173c08ebf1b445694185bb77542fc2
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-02-26 18:34:28 +0300

    updated

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import express from "express";
 
 const app = express();
 
-app.get("/api/hello", (req: express.Request, res: express.Response) => {
+app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello from the TypeScript backend!" });
 });
+
 if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
